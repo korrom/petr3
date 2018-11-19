a926ee5206ecfdbfc27bbb3556c39d0a832e5e52
Roman Korolchuk <kororom@bk.ru>
2018-11-19 15:38:35 +0300

    add plus

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,9 @@
 public class Calculate{
 	public static void main(String[] arg){
 		System.out.println("Calculate...");
-		
+		int a = Integer.parseInt(arg[0]);
+		int b = Integer.parseInt(arg[1]);
+		int sum = a + b;
+		System.out.println("Sum= " + sum);
 	}
 }
